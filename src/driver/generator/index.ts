3668007c4f27910362fc21e71018ae42c3eec6fe
ylc395 <cyl@cyl.moe>
2021-09-02 20:09:55 +0800

    reconstruct generator

--- src/driver/generator/index.ts
+++ src/driver/generator/index.ts
@@ -1,9 +1,10 @@
 import { container } from 'tsyringe';
 import ejs from 'ejs';
-import _, { filter, mapValues } from 'lodash';
+import _, { filter, mapValues, pick } from 'lodash';
 import joplin from 'api';
 import type { readFileSync as IReadFileSync, outputFile as IOutputFile } from 'fs-extra';
 import type { Site } from '../../domain/model/Site';
+import type { Article } from '../../domain/model/Article';
 import { ARTICLE_PAGE_NAME, INDEX_PAGE_NAME } from '../../domain/model/Page';
 import { defaultTheme, DEFAULT_THEME_NAME } from '../../domain/model/Theme';
 import { Db } from '../db';
@@ -13,71 +14,88 @@ const { readFileSync, outputFile } = joplin.require('fs-extra') as {
   readFileSync: typeof IReadFileSync;
   outputFile: typeof IOutputFile;
 };
+const db = container.resolve(Db);
 
 ejs.fileLoader = readFileSync;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Data = Readonly<Record<string, any>>;
 
+async function getSite() {
+  const site = await db.fetch<Site>(['site']);
+  const articles = filter((await db.fetch<Article[]>(['articles'])) || [], { published: true });
+
+  if (!site) {
+    throw new Error('no site info in db.json');
+  }
+
+  site.articles = articles;
+  site.generatedAt = Date.now();
+
+  return site as Required<Site>;
+}
+
+async function getThemeData(themeName: string) {
+  const themeConfig = themeName === DEFAULT_THEME_NAME ? defaultTheme : await loadTheme(themeName);
+
+  if (!themeConfig) {
+    throw new Error(`fail to load theme config: ${themeName}`);
+  }
+
+  const pagesFieldVars = (await db.fetch<Data>(['pagesFieldVars', themeName])) || {};
+  const defaultFieldVars = mapValues(themeConfig.pages, (fields) => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return fields!.reduce((vars, field) => {
+      vars[field.name] = field.defaultValue || '';
+      return vars;
+    }, {} as Record<string, unknown>);
+  });
+
+  return { pagesFieldVars, pages: themeConfig.pages, defaultFieldVars };
+}
+
+async function outputPage(
+  pageName: string,
+  pluginDir: string,
+  values: Record<string, unknown>,
+  site: Required<Site>,
+) {
+  const templatePath = `${pluginDir}/themes/${site.themeName}/templates/${pageName}.ejs`;
+  const siteData = { ...pick(site, ['generateAt', 'articles']), ...site.custom[site.themeName] };
+
+  if (pageName === ARTICLE_PAGE_NAME) {
+    for (const article of site.articles) {
+      const htmlString = await ejs.renderFile(
+        templatePath,
+        { ...values, $article: article, $site: siteData, _ },
+        { async: true },
+      );
+      await outputFile(
+        `${pluginDir}/output/${values.url || pageName}/${article.url}.html`,
+        htmlString,
+      );
+    }
+  } else {
+    const htmlString = await ejs.renderFile(
+      templatePath,
+      { ...values, $site: siteData, _ },
+      { async: true },
+    );
+    await outputFile(
+      `${pluginDir}/output/${pageName === INDEX_PAGE_NAME ? 'index' : values.url || pageName}.html`,
+      htmlString,
+    );
+  }
+}
+
 export default async function () {
   try {
-    const db = container.resolve(Db);
-    const site = await db.fetch<Site>(['site']);
-
-    if (!site) {
-      throw new Error('no site info in db.json');
-    }
-
-    const themeConfig =
-      site.themeName === DEFAULT_THEME_NAME ? defaultTheme : await loadTheme(site.themeName);
-
-    if (!themeConfig) {
-      throw new Error(`fail to load theme config: ${site.themeName}`);
-    }
-
-    const articles = filter((await db.fetch<Data[]>(['articles'])) || [], { published: true });
-    const pagesFieldVars = (await db.fetch<Data>(['pagesFieldVars', site.themeName])) || {};
-    const { pages } = themeConfig;
-    const defaultFieldVars = mapValues(pages, (fields) => {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      return fields!.reduce((vars, field) => {
-        vars[field.name] = field.defaultValue || '';
-        return vars;
-      }, {} as Record<string, unknown>);
-    });
+    const site = await getSite();
+    const { pages, pagesFieldVars, defaultFieldVars } = await getThemeData(site.themeName);
     const pluginDir = await joplin.plugins.dataDir();
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    site.articles = articles as any;
-
     for (const pageName of Object.keys(pages)) {
-      const fieldVars = { ...defaultFieldVars[pageName], ...(pagesFieldVars[pageName] || {}) };
-      const templatePath = `${pluginDir}/themes/${site.themeName}/templates/${pageName}.ejs`;
-
-      if (pageName === ARTICLE_PAGE_NAME) {
-        for (const article of articles) {
-          const htmlString = await ejs.renderFile(
-            templatePath,
-            { ...fieldVars, $article: article, $site: site, _ },
-            { async: true },
-          );
-          await outputFile(
-            `${pluginDir}/output/${fieldVars.url || pageName}/${article.url}.html`,
-            htmlString,
-          );
-        }
-      } else {
-        const htmlString = await ejs.renderFile(
-          templatePath,
-          { ...fieldVars, $site: site, _ },
-          { async: true },
-        );
-        await outputFile(
-          `${pluginDir}/output/${
-            pageName === INDEX_PAGE_NAME ? 'index' : fieldVars.url || pageName
-          }.html`,
-          htmlString,
-        );
-      }
+      const fieldValues = { ...defaultFieldVars[pageName], ...pagesFieldVars[pageName] };
+      await outputPage(pageName, pluginDir, fieldValues, site);
     }
   } catch (error) {
     console.warn(error);
